76ffcfc2e6ed7f1348fa31071c6965b4b44954f3
Class Account <ee106b-aaq@c105-7.local>
2025-03-17 19:39:51 -0700

    kalman

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/cc/ee106b/sp25/class/ee106b-aaq/project-3-dronesrcool/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/share/b/grading/bin:/home/ff/ee106b/bin:/home/ff/ee106b/bin/arch/x86_64:/share/b/grading/bin:/home/ff/ee106b/bin:/home/ff/ee106b/bin/arch/x86_64:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/share/b/grading/bin:/share/b/grading/sbin:/share/b/runas/x86_64:/share/b/bin:/home/cc/ee106b/sp25/class/ee106b-aaq/.vscode/extensions/ms-python.debugpy-2025.0.1-linux-x64/bundled/scripts/noConfigScripts:/share/b/grading/bin:/share/b/grading/sbin:/share/b/runas/x86_64:/share/b/bin'
+export PWD='/home/cc/ee106b/sp25/class/ee106b-aaq/project-3-dronesrcool/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/cc/ee106b/sp25/class/ee106b-aaq/project-3-dronesrcool/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/cc/ee106b/sp25/class/ee106b-aaq/project-3-dronesrcool/src:$ROS_PACKAGE_PATH"
